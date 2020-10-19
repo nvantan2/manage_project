3ef9edcf88e566b337156902548bba48282ea2f3
nvantan2 <tannguyen12.07.sk@gmail.com>
2020-10-19 15:22:12 +0700

    setup request base

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,22 +4,24 @@
  */
 import { extend } from 'umi-request';
 import { notification } from 'antd';
+import Qs from 'qs';
+
 const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
+  200: 'The server successfully returned the requested data. ',
+  201: 'Create or modify data successfully. ',
+  202: 'A request has entered the background queue (asynchronous task). ',
+  204: 'Delete data successfully. ',
+  400: 'There is an error in the request sent, and the server did not create or modify data. ',
+  401: 'The user does not have permission (the token, username, password are wrong). ',
+  403: 'The user is authorized, but access is forbidden. ',
+  404: 'The request sent is for a record that does not exist, and the server is not operating. ',
+  406: 'The requested format is not available. ',
+  410: 'The requested resource has been permanently deleted and will no longer be available. ',
+  422: 'When creating an object, a validation error occurred. ',
+  500: 'An error occurred in the server, please check the server. ',
+  502: 'Gateway error. ',
+  503: 'The service is unavailable, the server is temporarily overloaded or maintained. ',
+  504: 'The gateway has timed out. ',
 };
 /**
  * 异常处理程序
@@ -32,13 +34,13 @@ const errorHandler = (error) => {
     const errorText = codeMessage[response.status] || response.statusText;
     const { status, url } = response;
     notification.error({
-      message: `请求错误 ${status}: ${url}`,
+      message: `Request error ${status}: ${url}`,
       description: errorText,
     });
   } else if (!response) {
     notification.error({
-      description: '您的网络发生异常，无法连接服务器',
-      message: '网络异常',
+      description: 'Your network is abnormal and cannot connect to the server',
+      message: 'Network abnormal',
     });
   }
 
@@ -50,7 +52,7 @@ const errorHandler = (error) => {
 
 const request = extend({
   errorHandler,
-  // 默认错误处理
-  credentials: 'include', // 默认请求是否带上cookie
+  prefix: 'https://5e05c2032f5dff0014f7dd4f.mockapi.io',
+  paramsSerializer: (params) => Qs.stringify(params, { arrayFormat: 'brackets' }),
 });
 export default request;
