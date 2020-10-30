b5ddee2b82024e53e987dad9c857a1b8c15aaf2a
nvantan2 <tannguyen12.07.sk@gmail.com>
2020-10-30 08:09:12 +0700

    chore

--- src/pages/Boards/[id]/components/column.jsx
+++ src/pages/Boards/[id]/components/column.jsx
@@ -49,6 +49,7 @@ const Column = (props) => {
 
   const handleBlurTitleNewColumn = () => {
     const newTitle = html2Value(titleNewColumn.current).trim();
+
     if (!newTitle || newTitle === props.column.title) {
       titleNewColumn.current = props.column.title;
       setIsEditTitleColumn(false);
